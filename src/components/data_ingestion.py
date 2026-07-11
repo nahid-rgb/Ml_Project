05d1a23e817a43b4fc1b0f71bf25031024914d6e
Mahfuz <rmahfuzur818@gmail.com>
2026-07-11 17:11:00 +0600

    Model Trainer and Hyperparameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,8 +11,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # Stores all file paths used during data ingestion.
 # @dataclass automatically creates __init__(), making it ideal
@@ -27,7 +27,7 @@ class DataIngestionConfig:
 class DataIngestion:
     def __init__(self):
         # NOTE: calling DataIngestionConfig() only creates strings in memory.
-#       No folder, no file, nothing is written to disk here.
+        # No folder, no file, nothing is written to disk here.
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
@@ -71,4 +71,7 @@ if __name__ =='__main__':
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,11 @@ import numpy as np
 import pandas as pd 
 import dill
 import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
@@ -22,3 +26,49 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
             
+
+
+
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+            
+            # Train and evaluate each model
+        for name,model in models.items():
+
+                # Get hyperparameters for the current model
+                para = param[name]
+
+                # Perform Grid Search to find the best hyperparameters
+                gs = GridSearchCV(model,para,cv=3)
+                gs.fit(X_train, y_train)
+
+
+                # Update model with best hyperparameters
+                # e.g. RandomForestRegressor(n_estimators=64)
+                # Notice: This line does not train the model. It only changes its parameter values.
+
+                model.set_params(**gs.best_params_)
+
+                # Train the model again using the best parameters
+                # This trains the original model, but using the best hyperparameters found by GridSearchCV.
+                model.fit(X_train, y_train)
+
+                # Predictions
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
+
+                # Calculate R² scores
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
+
+
+
+                # Store only the test R² score
+                report[name] = test_model_score
+                
+        return report
+
+
+    except Exception as e:
+            raise CustomException(e,sys)      
